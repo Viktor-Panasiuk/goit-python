3795d3f77889901e85f7a4d48e1098d0d5a06b3d
unknown <vo151184pvv@gmail.com>
2020-12-14 23:58:13 +0200

    home_work1-2

--- lesson1/hw2.py
+++ lesson1/hw2.py
@@ -2,7 +2,7 @@ a = int(input("a = "))
 b = int(input("b = "))
 c = int(input("c = "))
 D = b**2 - 4*a*c
-x1 = (-b + D**(1/2))/2*a
-x2 = (-b - D**(1/2))/2*a
+x1 = (-b + D**(1/2))/(2*a)
+x2 = (-b - D**(1/2))/(2*a)
 print(f"x1 = {x1}")
 print(f"x2 = {x2}")
