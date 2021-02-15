6d63a5fb53c40688a57f847ab8c2072ec0c9211e
Viktor Panasiuk <vo151184pvv@gmail.com>
2021-02-15 22:51:47 +0200

    lesson7 v2

--- lesson7/clean_folder/setup.py
+++ lesson7/clean_folder/setup.py
@@ -6,5 +6,6 @@ setup(name='clean_folder',
     author='Viktor Panasjuk',
     author_email='vo151184pvv@gmail.com',
     license='MIT',
-    packeges=find_namespace_packages(),
+    packages=find_namespace_packages(),
+    include_package_data=True,
     entry_points={'console_scripts':['clean-folder = clean_folder.clean:clean_f']})
