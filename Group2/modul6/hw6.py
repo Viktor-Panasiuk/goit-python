6814cf4890c152499a16bc0aba488c315a939f09
Viktor Panasiuk <vo151184pvv@gmail.com>
2021-05-17 16:54:04 +0300

    Homework 6 v1.1

---
+++ Group2/modul6/hw6.py
@@ -0,0 +1,102 @@
+import re, sys, shutil
+
+from pathlib import Path 
+
+
+FILES_TO_SORTED = {'images':['JPEG', 'PNG', 'JPG', 'SVG'],
+                    'documents':['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX', 'XLS'],
+                    'audio':['MP3', 'OGG', 'WAV', 'AMR'],
+                    'video':['AVI', 'MP4', 'MOV', 'MKV'],
+                    'archives':['ZIP', 'GZ', 'TAR', 'RAR']}
+
+
+def create_folder(path_name):
+    #create folders (archives, video, audio, documents, images)
+    path_ = Path(path_name)
+    if not path_.exists():
+        path_.mkdir()
+
+
+def normalize_name(file_n):
+    
+    def transliterate(char):
+        map = {ord('А'):"A", ord('Б'):"B", ord('В'):"V", ord('Г'):"G", ord('Д'):"D", ord('Е'):"E", ord('Є'):"JE", ord('Ж'):"ZH", ord('З'):"Z",
+        ord('И'):"Y", ord('І'):"I", ord('Ї'):"JI", ord('Й'):"J", ord('К'):"K", ord('Л'):"L", ord('М'):"M", ord('Н'):"N", ord('О'):"O", ord('П'):"P",
+        ord('Р'):"R", ord('С'):"S", ord('Т'):"T", ord('У'):"U", ord('Ф'):"F", ord('Х'):"H", ord('Ц'):"C", ord('Ч'):"CH", ord('Ш'):"SH", ord('Щ'):"SHH",
+        ord('Ю'):"JU", ord('Я'):"JA", ord('Ь'):"'", ord('Ё'):"JO", ord('Ы'):"Y", ord('Э'):"JE"}
+        
+        if ord(char) in range(1040, 1104):
+            return  char.translate(map) if char.isupper() else char.upper().translate(map).lower()
+        else:
+            return char
+    
+    
+    name = file_n.name
+    suffix = file_n.suffix
+    text = name[:-len(suffix)] if file_n.is_file() else name
+    result = ''
+    for s in text:
+        result += transliterate(s)
+
+    name = re.sub('\W', '_', result) + suffix
+    try:
+        file_n = file_n.rename(file_n.with_name(name))
+    except Exception as E:
+        print(f'Error:\n{E}')
+    return file_n
+
+def unpack_archive(path_name):
+    for f in path_name.iterdir():
+        try:
+            shutil.unpack_archive(f, f.parent)
+            f.unlink()
+        except Exception as E:
+            print(f'Error:\n{E}')
+            
+
+
+def move_files(path_name, default_path, files_to_sorted):
+    
+    for f in path_name.iterdir():
+        f_n = normalize_name(f)
+        if f_n.is_file():
+            ext = f_n.suffix[1:].upper()
+            for keys in files_to_sorted.keys():
+                if ext in files_to_sorted[keys]:
+                    try:
+                        f_n.rename(Path(default_path + '/' + keys + '/' + f_n.name))
+                    except Exception as E:
+                        print(f'Error:\n{E}')
+                    break
+        else:
+            if not f_n.name in files_to_sorted.keys():
+                move_files(f_n, default_path, files_to_sorted)
+    # Delete dir
+    if len(list(path_name.iterdir())) == 0:
+        path_name.rmdir()
+
+
+
+def main():
+    try:
+        path_name = sys.argv[1]
+        path_name = path_name[:-1] if (path_name[-1] == '\\') or (path_name[-1] == '/') else path_name
+    except:
+        print("No argument")
+    else:
+        print(f"Start: {path_name}\n")
+        path_ = Path(path_name)
+        if not path_.exists():
+            print(f"The path: {path_name} dosn't exists!")
+        else:
+            # Create default folders
+            for f in FILES_TO_SORTED.keys():
+                create_folder(path_name + '/' + f)
+            
+            move_files(path_, path_name, FILES_TO_SORTED)
+            unpack_archive(Path(path_name + '/archives'))
+        print('Finish')
+
+
+if __name__ == '__main__':
+    main()
